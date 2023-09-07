5d0ba1561ca449b17e00b7bc96b4e17f50d22e3b
RehanRabbani <rabbanirehan6@gmail.com>
2023-09-07 13:19:15 +0500

    [TSK-001] add task-item

--- src/components/MainBoard/MainBoard.vue
+++ src/components/MainBoard/MainBoard.vue
@@ -1,98 +1,76 @@
 <template>
-   <div class="board-container">
+  <div class="board-container">
     <section class="pending">
-<h3>Pending</h3>
-<div class="col-3">
-      
-        <draggable
-          class="list-group"
-          :list="list1"
-          group="people"
-          @change="log"
-          itemKey="name"
-          v-bind="dragOptions"
-        >
-          <template #item="{ element, index }">
-            <div class="list-group-item">{{ element.name }} {{ index }}</div>
-          </template>
-        </draggable>
-      </div>
+      <h3>Pending</h3>
+
+      <draggable class="list-group" :list="list1" group="people" @change="log" itemKey="name" v-bind="dragOptions">
+        <template #item="{ element }">
+          <task-item :title="element.name" />
+        </template>
+      </draggable>
+
     </section>
     <section class="processing ">
-<h3>Proceesing</h3>
-<div class="col-3">
-      
-        <draggable
-          class="list-group"
-          :list="list2"
-          group="people"
-          @change="log"
-          itemKey="name"
-          v-bind="dragOptions"
-        >
-          <template #item="{ element, index }">
-            <div class="list-group-item">{{ element.name }} {{ index }}</div>
-          </template>
-        </draggable>
-      </div>
+      <h3>Proceesing</h3>
+      <draggable class="list-group" :list="list2" group="people" @change="log" itemKey="name" v-bind="dragOptions">
+        <template #item="{ element }">
+          <task-item :title="element.name" />
+        </template>
+      </draggable>
+
     </section>
     <section class="done">
-<h3>Done</h3>
-<div class="col-3">
-      
-        <draggable
-          class="list-group"
-          :list="list3"
-          group="people"
-          @change="log"
-          itemKey="name"
-          v-bind="dragOptions"
-        >
-          <template #item="{ element, index }">
-            <div class="list-group-item">{{ element.name }} {{ index }}</div>
-          </template>
-        </draggable>
-      </div>
+      <h3>Done</h3>
+      <draggable class="list-group" :list="list3" group="people" @change="log" itemKey="name" v-bind="dragOptions">
+        <template #item="{ element }">
+          <task-item :title="element.name" />
+        </template>
+      </draggable>
     </section>
-   </div>
-  </template>
-  <script>
-  import draggable from "vuedraggable";
-  
-  export default {
-    name: "two-lists",
-    display: "Two Lists",
-    order: 1,
-    components: {
-      draggable
-    },
-    data() {
-      return {
-        list1: [
-          { name: "John", id: 1 },
-          { name: "Joao", id: 2 },
-          { name: "Jean", id: 3 },
-          { name: "Gerard", id: 4 }
-        ],
-        list2: [
-          { name: "Juan", id: 5 },
-          { name: "Edgard", id: 6 },
-          { name: "Johnson", id: 7 }
-        ],
-        list3: [
-          { name: "rehan", id: 8 },
-          { name: "hage", id: 9 },
-          { name: "hafeez", id: 10 }
-        ]
-      };
-    },
-    methods: {
-     
-      log: function(evt) {
-        window.console.log(evt);
-      }
-    },
-    computed: {
+  </div>
+</template>
+<script setup>
+import TaskItem from "../TaskItem/TaskItem.vue";
+</script>
+
+<script >
+import draggable from "vuedraggable";
+
+
+export default {
+  name: "two-lists",
+  display: "Two Lists",
+  order: 1,
+  components: {
+    draggable
+  },
+  data() {
+    return {
+      list1: [
+        { name: "a", id: 1 },
+        { name: "b", id: 2 },
+        { name: "c", id: 3 },
+        { name: "d", id: 4 }
+      ],
+      list2: [
+        { name: "e", id: 5 },
+        { name: "f", id: 6 },
+        { name: "g", id: 7 }
+      ],
+      list3: [
+        { name: "rehan", id: 8 },
+        { name: "hage", id: 9 },
+        { name: "hafeez", id: 10 }
+      ]
+    };
+  },
+  methods: {
+
+    log: function (evt) {
+      window.console.log(evt);
+    }
+  },
+  computed: {
     dragOptions() {
       return {
         animation: 400,
@@ -100,10 +78,10 @@
         disabled: false,
         ghostClass: "ghost"
       };
-    }}
-  };
-  </script>
+    }
+  }
+};
+</script>
 
-<style src="./MainBoard.css" scoped>
-</style>
+<style src="./MainBoard.css" scoped></style>
   
---
+++ src/components/TaskItem/TaskItem.vue
@@ -0,0 +1,35 @@
+<template >
+<div class="task-container">
+    <section class="task-header">
+       <h5> {{ title }}  </h5>
+       <span>
+        edit
+        delete  
+       </span>
+    </section>
+    <section>
+        <p>Task</p>
+        <p>description</p>
+        <p>Estimated Time</p>
+        <p>Attachements</p>
+    </section>
+</div>
+  
+</template>
+
+<script setup>
+defineProps(['title'])
+</script>
+
+<style scoped>
+.task-header{
+    width: 228px;
+    background-color: aquamarine;
+
+}
+.task-container{
+    width: 230px;
+    background-color: azure;
+    border: 1px solid;
+}
+</style>
