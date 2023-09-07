c912b879c287e31762bdbd8415c266284571b0f0
RehanRabbani <rabbanirehan6@gmail.com>
2023-09-07 14:02:11 +0500

    [TSK-002] get data from store

--- src/components/MainBoard/MainBoard.vue
+++ src/components/MainBoard/MainBoard.vue
@@ -2,22 +2,19 @@
   <div class="board-container">
     <section class="pending">
       <h3>Pending</h3>
-
       <draggable class="list-group" :list="list1" group="people" @change="log" itemKey="name" v-bind="dragOptions">
         <template #item="{ element }">
           <task-item :title="element.name" />
         </template>
       </draggable>
-
     </section>
-    <section class="processing ">
+    <section class="processing">
       <h3>Proceesing</h3>
       <draggable class="list-group" :list="list2" group="people" @change="log" itemKey="name" v-bind="dragOptions">
         <template #item="{ element }">
           <task-item :title="element.name" />
         </template>
       </draggable>
-
     </section>
     <section class="done">
       <h3>Done</h3>
@@ -31,38 +28,31 @@
 </template>
 <script setup>
 import TaskItem from "../TaskItem/TaskItem.vue";
+import useAppStore from '../../store/app';
+const app = useAppStore();
+const list1 = app.list1;
+const list2 = app.list2;
+const list3 = app.list3;
+const setup = () => {
+  return {
+    list1,
+    list2,
+    list3
+  };
+}
+
+
+
 </script>
 
 <script >
 import draggable from "vuedraggable";
-
-
 export default {
   name: "two-lists",
   display: "Two Lists",
   order: 1,
   components: {
     draggable
-  },
-  data() {
-    return {
-      list1: [
-        { name: "a", id: 1 },
-        { name: "b", id: 2 },
-        { name: "c", id: 3 },
-        { name: "d", id: 4 }
-      ],
-      list2: [
-        { name: "e", id: 5 },
-        { name: "f", id: 6 },
-        { name: "g", id: 7 }
-      ],
-      list3: [
-        { name: "rehan", id: 8 },
-        { name: "hage", id: 9 },
-        { name: "hafeez", id: 10 }
-      ]
-    };
   },
   methods: {
 
--- src/store/app.js
+++ src/store/app.js
@@ -1,8 +1,25 @@
 // Utilities
-import { defineStore } from 'pinia'
+import { defineStore } from "pinia";
 
-export const useAppStore = defineStore('app', {
+export const useAppStore = defineStore("app", {
   state: () => ({
-    //
+    list1: [
+      { name: "a", id: 1 },
+      { name: "b", id: 2 },
+      { name: "c", id: 3 },
+      { name: "d", id: 4 },
+    ],
+    list2: [
+      { name: "e", id: 5 },
+      { name: "f", id: 6 },
+      { name: "g", id: 7 },
+    ],
+    list3: [
+      { name: "rehan", id: 8 },
+      { name: "hage", id: 9 },
+      { name: "hafeez", id: 10 },
+    ],
   }),
-})
+});
+
+export default useAppStore;
