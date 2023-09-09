74d958d87eec883445674cb23ff739f35bc5a373
RehanRabbani <rabbanirehan6@gmail.com>
2023-09-09 18:46:55 +0500

    [TSK-011] updated

--- src/components/TaskAddPage/TaskAddPage.vue
+++ src/components/TaskAddPage/TaskAddPage.vue
@@ -2,22 +2,32 @@
 <template>
     <div class="task-page">
         <section class="col-1">
-            <section class="name">
-                <p>Create Task</p>
-                <v-btn color="primary" @click="dialog = false">Close</v-btn>
+            <section class="task-creation">
+                <h1 class="jira-heading">Create Task</h1>
+                <v-btn color="primary" @click="closePage" class="jira-button">Close</v-btn>
             </section>
-            <v-text-field label="Title" v-model="newTask.title"></v-text-field>
-            <v-textarea label="Description" v-model="newTask.desc"></v-textarea>
-            <v-text-field label="Date" :type="'date'" v-model="newTask.time"></v-text-field>
-            <file-upload @data-to-parent="handleDataFromChild" />
-
-            <v-btn @click="addTask">
-                Add
-            </v-btn>
+            <div class="jira-form-field">
+                <v-text-field label="Title" v-model="newTask.title"></v-text-field>
+            </div>
+            <div class="jira-form-field">
+                <v-textarea label="Description" v-model="newTask.desc"></v-textarea>
+            </div>
+            <div class="jira-form-field">
+                <v-text-field label="Date" :type="'date'" v-model="newTask.time"></v-text-field>
+            </div>
+            <div class="jira-form-field">
+                <file-upload @data-to-parent="handleDataFromChild" />
+            </div>
+            <div class="jira-buttons">
+                <v-btn class="jira-button" @click="addTask" v-if="newTask.status !== ''">
+                    Add
+                </v-btn>
+            </div>
         </section>
         <section class="col-2">
-            <v-select label="Select" v-model="newTask.status" :items="['Pending', 'Processing', 'Done']"></v-select>
-            <v-autocomplete label="Autocomplete" v-model="newTask.tag"
+            <v-select label="Select" v-model="newTask.status" :items="['Pending', 'Processing', 'Done']"
+                :rules="statusRules"></v-select>
+            <v-autocomplete label="Tag" v-model="newTask.tag"
                 :items="['release 3', 'release 2', 'release 3']"></v-autocomplete>
         </section>
 
@@ -25,9 +35,10 @@
 </template>
 <script setup>
 import FileUpload from '../FileUpload/FileUpload.vue';
-import { reactive } from 'vue';
+import { reactive, ref } from 'vue';
 import useAppStore from '@/store/app';
-
+import { useRouter } from 'vue-router';
+const router = useRouter();
 const appStore = useAppStore();
 const newTask = reactive({
     title: '',
@@ -38,14 +49,23 @@ const newTask = reactive({
     time: ''
 })
 
+const statusRules = ref([
+    v => !!v || 'Status is required',
+]);
+
 const addTask = () => {
     const { title, desc, file, status, tag, time } = newTask
     appStore.addTask({ title, desc, file, time, status, tag, id: Date.now() })
+    router.push({ name: 'Home' });
 };
 
 const handleDataFromChild = (data) => {
     newTask.file = [data]
-}
+};
+
+const closePage = () => {
+    router.push({ name: 'Home' });
+};
 
 </script>
 <style src="./TaskAddPage.css" scoped></style>
